a70caf8a995fe97e336799509c629fee22be3a9c
suzannasim <suzannasim@gmail.com>
2017-11-23 21:55:22 -0800

    update 11/23/2017

---
+++ javascript/js.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    Typed.new('#typed', {
+        strings: ["an experience seeker.", "a knowledge consumer.", "an anything tryer.", "a creativity enthusiast."],
+        typeSpeed: 80,
+        backDelay: 800,
+        loop: true
+    });
+});
+
