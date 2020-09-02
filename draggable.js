3df0a9625a623248ecbccac561abbc1bb5358998
suzannasim <suzannasim@gmail.com>
2020-09-01 23:14:20 -0700

    website 2.0

---
+++ draggable.js
@@ -0,0 +1,6 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+$(function(){
+    $('.blob').draggable({scroll:false});
+});
--- javascript/js.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    Typed.new('#typed', {
-        strings: ["an experience seeker.", "a knowledge pursuer.", "a curiosity enthusiast."],
-        typeSpeed: 80,
-        backDelay: 800,
-        loop: true
-    });
-});
-
