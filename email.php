55a073a44ce4b8511e85009f9b5da839d47be6cc
Suzanna Sim <suzannasim@gmail.com>
2020-09-08 12:11:13 -0700

    hardcoded email form testing

--- email.php
+++ email.php
@@ -1,27 +1,34 @@
 <?php
 
-if(!isset($_POST['submit'])) {
-    //This page should not be accessed directly. Need to submit the form.
-    echo "error; you need to submit the form";
-}
+//if(!isset($_POST['submit'])) {
+//    //This page should not be accessed directly. Need to submit the form.
+//    echo "error; you need to submit the form";
+//}
+//
+//$name = $_POST['name'];
+//$visitor_email = $_POST['email'];
+//$subject = $_POST['subject'];
+//$message = $_POST['message'];
+//
+////Validate first
+//if(empty($name) || empty($visitor_email)) {
+//    echo "name and email are mandatory";
+//        exit;
+//}
 
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
-
-//Validate first
-if(empty($name) || empty($visitor_email)) {
-    echo "name and email are mandatory";
-        exit;
-}
+//$email_from = "suzannasim@gmail.com";
+//$email_subject = "Website Form Submission: $name, $subject";
+//$email_body = "$message";
+//$to = "suzannasim@gmail.com";
+//$headers = "From: $email_from \r\n";
+//$headers .= "Reply-To: $visitor_email \r\n";
 
 $email_from = "suzannasim@gmail.com";
-$email_subject = "Website Form Submission: $name, $subject";
-$email_body = "$message";
+$email_subject = "Website Form Submission";
+$email_body = "It worked!";
 $to = "suzannasim@gmail.com";
-$headers = "From: $email_from \r\n";
-$headers .= "Reply-To: $visitor_email \r\n";
+$headers = "From: suzannasim@gmail.com \r\n";
+$headers .= "Reply-To: kdquon@gmail.com \r\n";
 
 mail($to, $email_subject, $email_body, $headers);
 
