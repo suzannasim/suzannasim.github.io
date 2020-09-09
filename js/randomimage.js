b75d542db77e59f61f3b2a6032ade261e7bd1bd8
Suzanna Sim <suzannasim@gmail.com>
2020-09-09 15:23:03 -0700

    new blob images

--- js/randomimage.js
+++ js/randomimage.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $(document).click(function(e){
-    var myPix = new Array("img/Blobb1.png", "img/Blobb2.png", "img/Blob3.png", "img/Blob4.png");
+    var myPix = new Array("img/Blob1.png", "img/Blob2.png", "img/New_Blob3.png", "img/New_Blob4.png", "img/New_Blob5.png", "img/New_Blob6.png");
     var num = Math.floor((Math.random() * myPix.length));
       console.log(myPix[num]);
     var img = $('<img class="blob" src="' + myPix[num] + '"/>');
